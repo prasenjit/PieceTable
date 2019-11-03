a41eaefff869131016e0ee683ebdcf1a5c249ff8
Prasenjit Kumar Shaw <shawprasenjit07@gmail.com>
2019-11-03 16:30:03 +0530

    Add undo/redo functions for Piece Table

--- piecetable.py
+++ piecetable.py
@@ -12,6 +12,9 @@ class PieceTable:
                 "startpos": 0,
                 "length": len(self.origBuff)
             }]
+        self.recievedCommands = []
+        self.undoStack = []
+        self.redoStack = []
 
     def resetBuffers(self):
         self.origBuff = self.getOutputText()
@@ -21,6 +24,9 @@ class PieceTable:
                 "startpos": 0,
                 "length": len(self.origBuff)
             }]
+        self.recievedCommands = []
+        self.undoStack = []
+        self.redoStack = []
 
     def __getPieceIndex(self, startpos):  # return piece index and offset within this piece
         offset = startpos
@@ -30,11 +36,67 @@ class PieceTable:
             offset -= piece["length"]
         return 0, 0
 
-    def insert(self, string, startpos):
+    def __convertCommand(self, command):
+        convertedCommand = {}
+        if command["type"] == "insert":
+            convertedCommand["type"] = "delete"
+            startpos = command["args"][1]
+            n_chars = len(command["args"][0])
+            convertedCommand["args"] = [startpos, n_chars]
+        else:
+            convertedCommand["type"] = "insert"
+            string = ''
+            startpos = command["args"][0]
+            convertedCommand["args"] = [string, startpos]
+        return convertedCommand
+        
+    def undo(self):
+        print("XXXXX Recieved Command: undo()")
+        try:
+            command = self.undoStack.pop()
+            self.redoStack.append(command)
+            convertedCommand = self.__convertCommand(command)
+            if convertedCommand["type"] == "insert":
+                self.insert(convertedCommand["args"][0], convertedCommand["args"][1], True)
+            else:
+                self.delete(convertedCommand["args"][0], convertedCommand["args"][1], True)
+            # self.undoStack.pop()
+        except:
+            return
+
+    def redo(self):
+        print("XXXXX Recieved Command: redo()")
+        try:
+            command = self.redoStack.pop()
+            self.undoStack.append(command)
+            convertedCommand = command
+            if convertedCommand["type"] == "insert":
+                self.insert(convertedCommand["args"][0], convertedCommand["args"][1], True)
+            else:
+                self.delete(convertedCommand["args"][0], convertedCommand["args"][1], True)
+            # self.redoStack.pop()
+        except:
+            return
+
+
+    def insert(self, string, startpos, internalCall=False):
+        print("XXXXX Recieved Command: insert({0}, {1})".format(string, startpos))
+        command = {
+            "type": "insert",
+            "args" : [string, startpos]
+        }
+        self.recievedCommands.append(command)
+        if not internalCall:
+            self.undoStack.append(command)
+
         if len(string) == 0:
             return
-        addBuffOffset = len(self.addBuff)
-        self.addBuff += string
+
+        if internalCall:
+            addBuffOffset = self.addBuff.find(string)
+        else:
+            addBuffOffset = len(self.addBuff)
+            self.addBuff += string
         pieceIndex, buffOffset = self.__getPieceIndex(startpos)
         piece = self.table[pieceIndex]
         if piece["add"] and buffOffset == (piece["startpos"] + piece["length"]) and addBuffOffset == (piece["startpos"] + piece["length"]):
@@ -65,13 +127,23 @@ class PieceTable:
         if piece1["length"] > 0:
             self.table.insert(pieceIndex, piece1)
 
-    def delete(self, startpos, n_chars):
+    def delete(self, startpos, n_chars, internalCall=False):
         if n_chars == 0:
             return
         if startpos < 0:
             return
         if n_chars < 0:
             return self.delete(startpos + n_chars, -n_chars)
+
+        if n_chars > 0:
+            print("XXXXX Received Command: delete({0}, {1})".format(startpos, n_chars))
+            command = {
+                "type": "delete",
+                "args" : [startpos, n_chars]
+            }
+            self.recievedCommands.append(command)
+            if not internalCall:
+                self.undoStack.append(command)
 
         initPieceIndex, initBuffOffset = self.__getPieceIndex(startpos)
         finalPieceIndex, finalBuffOffset = self.__getPieceIndex(startpos + n_chars)
@@ -124,72 +196,116 @@ class PieceTable:
             print("AddBuff: {0}\t\tStartPos: {1}\t\tLength: {2}".format(
                 piece["add"], piece["startpos"], piece["length"]
             ))
+
+        print("::: Command Stack :::")
+        for command in self.recievedCommands:
+            print("\t{0}".format(command))
+
+        print("::: Undo Stack :::")
+        for command in self.undoStack:
+            print("\t{0}".format(command))
+
+        print("::: Redo Stack :::")
+        for command in self.redoStack:
+            print("\t{0}".format(command))
         print("====================")
 
 
-if __name__ == '__main__':
+def example1():
     initString = "Hey, this is a program implementing Piece Table Method."
 
     print("Initialising...")
     table = PieceTable(initString)
     table.printPieceTable()
     print("Final Text: {0}".format(table.getOutputText()))
-    print()
+    print("=.=.=.=.=.=.=.=.=.=.=.=.=.=.=.=.=.=.=.=.=.=.=.=.=")
     
     print("Adding text...")
     table.insert(" for text editors", len(initString) - 1)
     table.printPieceTable()
     print("Final Text: {0}".format(table.getOutputText()))
-    print()
+    print("=.=.=.=.=.=.=.=.=.=.=.=.=.=.=.=.=.=.=.=.=.=.=.=.=")
 
     print("Adding text...")
     table.insert(" you", 3)
     table.printPieceTable()
     print("Final Text: {0}".format(table.getOutputText()))
-    print()
+    print("=.=.=.=.=.=.=.=.=.=.=.=.=.=.=.=.=.=.=.=.=.=.=.=.=")
 
-    print("\n\n\t\t{0}".format(table.getOutputText()[3:3+4]))
+    print("\n\nTo be deleted: {0}".format(table.getOutputText()[3:3+4]))
     print("Deleting text...")
     table.delete(3, 4)
     table.printPieceTable()
     print("Final Text: {0}".format(table.getOutputText()))
+    print("=.=.=.=.=.=.=.=.=.=.=.=.=.=.=.=.=.=.=.=.=.=.=.=.=")
 
-
-    print("\n\n\t\t{0}".format(table.getOutputText()[15-4:15]))
+    print("\n\nTo be deleted: {0}".format(table.getOutputText()[15-4:15]))
     print("Deleting text...")
     table.delete(15, -4)
     table.printPieceTable()
     print("Final Text: {0}".format(table.getOutputText()))
+    print("=.=.=.=.=.=.=.=.=.=.=.=.=.=.=.=.=.=.=.=.=.=.=.=.=")
 
     print("\n\nResetting Buffers...")
     table.resetBuffers()
     table.printPieceTable()
+    print("=.=.=.=.=.=.=.=.=.=.=.=.=.=.=.=.=.=.=.=.=.=.=.=.=")
 
     print("Adding text...")
     table.insert(" for text editors", len(initString) - 1)
     table.printPieceTable()
     print("Final Text: {0}".format(table.getOutputText()))
-    print()
+    print("=.=.=.=.=.=.=.=.=.=.=.=.=.=.=.=.=.=.=.=.=.=.=.=.=")
 
     print("Adding text...")
     table.insert(" you", 3)
     table.printPieceTable()
     print("Final Text: {0}".format(table.getOutputText()))
-    print()
+    print("=.=.=.=.=.=.=.=.=.=.=.=.=.=.=.=.=.=.=.=.=.=.=.=.=")
 
-    print("\n\n\t\t{0}".format(table.getOutputText()[3:3+4]))
+    print("\n\nTo be deleted: {0}".format(table.getOutputText()[3:3+4]))
     print("Deleting text...")
     table.delete(3, 4)
     table.printPieceTable()
     print("Final Text: {0}".format(table.getOutputText()))
+    print("=.=.=.=.=.=.=.=.=.=.=.=.=.=.=.=.=.=.=.=.=.=.=.=.=")
 
 
-    print("\n\n\t\t{0}".format(table.getOutputText()[15-4:15]))
+    print("\n\nTo be deleted: {0}".format(table.getOutputText()[15-4:15]))
     print("Deleting text...")
     table.delete(15, -4)
     table.printPieceTable()
     print("Final Text: {0}".format(table.getOutputText()))
+    print("=.=.=.=.=.=.=.=.=.=.=.=.=.=.=.=.=.=.=.=.=.=.=.=.=")
     
     print("\n\nResetting Buffers...")
     table.resetBuffers()
     table.printPieceTable()
+    print("=.=.=.=.=.=.=.=.=.=.=.=.=.=.=.=.=.=.=.=.=.=.=.=.=")
+
+def example2():
+    initString = 'ABCDEFGH'
+    table = PieceTable(initString)
+    table.printPieceTable()
+    print("Final Text: {0}".format(table.getOutputText()))
+    print("=.=.=.=.=.=.=.=.=.=.=.=.=.=.=.=.=.=.=.=.=.=.=.=.=")
+
+    table.insert("IJKL", len(initString))
+    table.printPieceTable()
+    print("Final Text: {0}".format(table.getOutputText()))
+    print("=.=.=.=.=.=.=.=.=.=.=.=.=.=.=.=.=.=.=.=.=.=.=.=.=")
+
+    table.undo()
+    table.printPieceTable()
+    print("Final Text: {0}".format(table.getOutputText()))
+    print("=.=.=.=.=.=.=.=.=.=.=.=.=.=.=.=.=.=.=.=.=.=.=.=.=")
+
+    table.redo()
+    table.printPieceTable()
+    print("Final Text: {0}".format(table.getOutputText()))
+    print("=.=.=.=.=.=.=.=.=.=.=.=.=.=.=.=.=.=.=.=.=.=.=.=.=")
+
+
+if __name__ == '__main__':
+    # example1()
+    example2()
